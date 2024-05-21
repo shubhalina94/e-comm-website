5976cd79eb28584e74b8ad6c505fe9d2d7b577b4
Shubhalina Mal <shubhalinamal@gmail.com>
2024-05-21 17:26:11 +0530

    third commit in backend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ const express=require('express')
 const cors =require('cors')
 require('dotenv').config()
 const connectDB=require('./config/db')
+const router=require('./routes')
+
 const app=express()
 app.use(cors())
+app.use("/api",router)
 
 const PORT=8080 || process.env.PORT
 
-connectDB()
-app.listen(PORT,()=>{
-    console.log("Server is running...")
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("COnnect to DB")
+        console.log("Server is running...")
+    })
 })
