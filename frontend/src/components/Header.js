a49d88ce5bab7bf03d4518420925f852a57b9433
Shubhalina Mal <shubhalinamal@gmail.com>
2024-06-14 23:58:28 +0530

    changes saved

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -75,6 +75,8 @@ const Header = () => {
                   <Link to={"/admin-panel/all-products"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
                 )
                 }
+
+                 
               
             </nav>
           </div>
